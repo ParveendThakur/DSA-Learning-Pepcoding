8e0191ca08d1d34bbc9e53a7c1a85df6a2e4b5e0
ParveendThaku <parveend1234@gmail.com>
2022-05-12 16:00:33 +0530

    Recursion start

--- printDecreasing.java
+++ printDecreasing.java
@@ -3,12 +3,17 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // write your code here
-    }
+        Scanner scn = new Scanner (System.in);
+        int n = scn.nextInt();
 
+        printDecreasing(n);
+    }
     public static void printDecreasing(int n){
-        
-    }
+        if (n==0) return;  // base case
 
+        System.out.println(n);
+      //  printDecreasing(n-1); // faith       
+    }
 }
