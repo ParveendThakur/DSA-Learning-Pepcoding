dca1c3e47935f4899166a2671a783e2254e4380b
ParveendThaku <parveend1234@gmail.com>
2022-05-25 18:19:34 +0530

    make a function for idx get

--- linkedListsForBeginners/reverseALinkedList(dataIterative).java
+++ linkedListsForBeginners/reverseALinkedList(dataIterative).java
@@ -155,6 +155,10 @@ public class Main {
       }
     }
 
+    public getNodeAt(int idx){
+      
+    }
+
     public void reverseDI() {
       // write your code here
       int left = 0 , right = this.size - 1;
