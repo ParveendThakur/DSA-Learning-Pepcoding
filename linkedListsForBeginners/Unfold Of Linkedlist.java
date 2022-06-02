b6b7ca67ea7cf36100b3d8dacb8934851ac1a9e3
ParveendThaku <parveend1234@gmail.com>
2022-06-02 18:15:38 +0530

    Q ADD YESTERDAY CLASS

---
+++ linkedListsForBeginners/Unfold Of Linkedlist.java
@@ -0,0 +1,39 @@
+// https://nados.io/question/unfold-of-linkedlist?zen=true
+import java.util.*;
+
+class Main {
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void unfold(ListNode head) {
+
+    }
+
+    static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+
+        ListNode head = dummy.next;
+        unfold(head);
+        printList(head);
+    }
+}
