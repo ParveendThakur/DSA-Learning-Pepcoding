d229ec35c315df0beef9dba72a32d0079311002f
ParveendThaku <parveend1234@gmail.com>
2022-06-03 22:35:49 +0530

    done remove first

--- linkedListsForBeginners/Remove First In Linkedlist.java
+++ linkedListsForBeginners/Remove First In Linkedlist.java
@@ -41,6 +41,18 @@ public class Main {
 
     public void removeFirst(){
       // write your code here
+      if (size == 0) System.out.println("List is empty");
+      else if(this.size == 1){
+        this.head = this.tail = null;
+        this.size = 0;
+      }else {
+        //Node nbr = this.head.next;
+        //this.head.next = null;
+        //this.head = nbr;
+        //this.size--;
+        head = head.next;
+        size--;
+      }
     }
   }
 
