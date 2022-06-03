ff39830e174c9615bea0b5a42e9ed6d61726caa7
ParveendThaku <parveend1234@gmail.com>
2022-06-03 18:44:21 +0530

    Q done

--- linkedListsForBeginners/Unfold Of Linkedlist.java
+++ linkedListsForBeginners/Unfold Of Linkedlist.java
@@ -1,4 +1,3 @@
-// https://nados.io/question/unfold-of-linkedlist?zen=true
 import java.util.*;
 
 class Main {
@@ -10,11 +9,51 @@ class Main {
             this.val = val;
         }
     }
-
-    public static void unfold(ListNode head) {
-
+    public static ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr!=null){
+            ListNode temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
     }
+    public static void unfold(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) 
+        return;
 
+        ListNode d1 = new ListNode(0);
+        ListNode d2 = new ListNode(1);
+
+        ListNode p1 = d1 , p2 = d2 , p3 = head;
+        boolean flag = true;
+        while (p3 != null){
+            if (flag ){
+                p1.next =p3;
+                p1 = p1.next;
+
+            }else {
+                p2.next = p3;
+                p2 = p2.next;
+            }
+            p3 = p3.next;
+            flag = !flag;
+        }
+        p1.next = null;
+        p2.next = null;
+
+        ListNode head2 = d2.next;
+        head2 = reverse(head2);
+
+        //ListNode ptr = head;
+        //while (ptr.next != null){
+        //    ptr = ptr.next;
+        //}
+        //ptr.next = head2;
+        p1.next= head2;
+    }
     static void printList(ListNode node) {
         while (node != null) {
             System.out.print(node.val + " ");
