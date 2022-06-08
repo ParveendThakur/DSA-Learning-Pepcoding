7f417c00a584f3e9d9eb1bdfaded77bfef22433d
ParveendThaku <parveend1234@gmail.com>
2022-06-09 01:23:05 +0530

    Done Q- dupicate....

--- Stacks And Queues For Beginners/Duplicate Brackets.java
+++ Stacks And Queues For Beginners/Duplicate Brackets.java
@@ -1,4 +1,3 @@
-// https://nados.io/question/duplicate-brackets?zen=true
 // #include <bits/stdc++.h>
 // using namespace std;
 // int main(int argc, char **argv)
@@ -8,3 +7,37 @@
 // }
 
 
+import java.util.*;
+public class Main{
+
+   public static void main(String args[]){
+      Scanner scn = new Scanner(System.in);
+
+      String exp = scn.nextLine();
+
+      System.out.println(duplicateBrackets(exp));
+   }
+
+   public static boolean duplicateBrackets(String exp){
+      Stack<Character> st = new Stack<>();
+
+      for(int i = 0 ; i < exp.length() ; i++){
+          char ch = exp.charAt(i);
+          if(ch == ' '){continue;}
+          else 
+          if(ch == ')'){
+              if(st.peek() == '('){
+                  return true;
+              }else{
+                  while(st.peek() != '('){
+                      st.pop();
+                  }
+                  st.pop(); // (
+              }
+          }else{
+              st.push(ch);
+          }
+      }
+      return false;
+   }
+}   
