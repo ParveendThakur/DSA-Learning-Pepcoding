562864c5d132d885c8a6e582fa89b6b0765013c2
ParveendThakur <parveend1234@gmail.com>
2022-06-20 21:52:08 +0530

    Done Q---

--- Generic Tree For Beginners/Lowest Common Ancestor (generic Tree).java
+++ Generic Tree For Beginners/Lowest Common Ancestor (generic Tree).java
@@ -63,8 +63,22 @@ public class Main {
     return new ArrayList<>();
   }
 
-  public static int lca(Node node, int d1, int d2) {
+  public static int lca(Node root, int d1, int d2) {
     // write your code here
+    ArrayList<Integer> path1 = nodeToRootPath(root,d1);
+    ArrayList<Integer> path2 = nodeToRootPath(root,d2);
+
+    int i = path1.size()-1 , j = path2.size()-1;
+
+    while (i >= 0 && j >= 0){
+      if (path1.get(i) == path2.get(j)){
+        i--;
+        j--;
+      }else{
+        break;
+      }
+    }
+    return path2.get(j+1);
   }
 
   public static void main(String[] args) throws Exception {
