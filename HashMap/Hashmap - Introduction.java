6c2b8d62302b96a16fe2f03cec4d29dd62603ac8
ParveendThakur <parveend1234@gmail.com>
2022-07-01 21:52:11 +0530

    Add value ---> Hash Map

---
+++ HashMap/Hashmap - Introduction.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class Main{
+    public static void main(String args[]){
+        HashMap<String,Integer> hm = new HashMap<>();
+        System.out.println(hm);
+        // add an ele
+        hm.put("India",200);
+        hm.put("China",190);
+        hm.put("Uk",170);
+        hm.put("USA",110);
+        System.out.println(hm);
+     
+    }
+}
