48881b3a863b5bfcba30305ac46fbbebbd4bea42
ParveendThakur <parveend1234@gmail.com>
2022-07-01 21:57:37 +0530

    KeySet Func.... ---> Hash Map

--- HashMap/Hashmap - Introduction.java
+++ HashMap/Hashmap - Introduction.java
@@ -9,20 +9,22 @@ public class Main{
         hm.put("China",190);
         hm.put("Uk",170);
         hm.put("USA",110);
-        System.out.println(hm);
-        // update a value
-        hm.put("India",150);
-        System.out.println(hm);
-        // key exists
-        System.out.println(hm.containsKey("India"));
-        System.out.println(hm.containsKey("india"));
-        // get a value
-        System.out.println(hm.get("India"));
-        System.out.println(hm.get("india"));
-        // remove a key,value
-        System.out.println(hm.remove("India")+" status -> "+hm);
-        System.out.println(hm.remove("India")+" status -> "+hm);
+        // System.out.println(hm);
+        // // update a value
+        // hm.put("India",150);
+        // System.out.println(hm);
+        // // key exists
+        // System.out.println(hm.containsKey("India"));
+        // System.out.println(hm.containsKey("india"));
+        // // get a value
+        // System.out.println(hm.get("India"));
+        // System.out.println(hm.get("india"));
+        // // remove a key,value
+        // System.out.println(hm.remove("India")+" status -> "+hm);
+        // System.out.println(hm.remove("India")+" status -> "+hm);
 
-        
+        for(String key : hm.keySet()){
+            System.out.println("key : "+key +" val : "+hm.get(key));
+        }
     }
 }
