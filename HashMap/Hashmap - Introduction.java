548334e2d80162fb410489c5385e57d71dcaff85
ParveendThakur <parveend1234@gmail.com>
2022-07-01 21:56:52 +0530

    Remove a key , value ---> Hash Map....

--- HashMap/Hashmap - Introduction.java
+++ HashMap/Hashmap - Introduction.java
@@ -19,6 +19,10 @@ public class Main{
         // get a value
         System.out.println(hm.get("India"));
         System.out.println(hm.get("india"));
+        // remove a key,value
+        System.out.println(hm.remove("India")+" status -> "+hm);
+        System.out.println(hm.remove("India")+" status -> "+hm);
+
         
     }
 }
