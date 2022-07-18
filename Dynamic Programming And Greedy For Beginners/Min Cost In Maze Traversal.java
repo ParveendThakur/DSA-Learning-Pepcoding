25fcff777369985681a94b8744510b67511ffee8
ParveendThakur <parveend1234@gmail.com>
2022-07-18 22:55:45 +0530

    driver code --DP

--- Dynamic Programming And Greedy For Beginners/Min Cost In Maze Traversal.java
+++ Dynamic Programming And Greedy For Beginners/Min Cost In Maze Traversal.java
@@ -1,1 +1,11 @@
 // https://nados.io/question/min-cost-in-maze-traversal
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+    }
+
+}
