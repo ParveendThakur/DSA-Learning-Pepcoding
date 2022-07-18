ab142daac7dfe6286633fd461b1ce4b4d0e5f349
ParveendThakur <parveend1234@gmail.com>
2022-07-18 22:01:27 +0530

    driver code -- push

--- Dynamic Programming And Greedy For Beginners/Climb Stairs With Variable Jumps.java
+++ Dynamic Programming And Greedy For Beginners/Climb Stairs With Variable Jumps.java
@@ -1,1 +1,11 @@
 //https://nados.io/question/climb-stairs-with-variable-jumps
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+    }
+
+}
