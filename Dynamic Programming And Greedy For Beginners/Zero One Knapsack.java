28f7f93eeff9bf724bcb59ce86804f31401db04f
ParveendThakur <parveend1234@gmail.com>
2022-07-21 23:50:20 +0530

    Driver code

--- Dynamic Programming And Greedy For Beginners/Zero One Knapsack.java
+++ Dynamic Programming And Greedy For Beginners/Zero One Knapsack.java
@@ -1,1 +1,10 @@
 // https://nados.io/question/zero-one-knapsack
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
