24e175db36286b36519aaa740d4740df6df677c2
ParveendThakur <parveend1234@gmail.com>
2022-07-25 23:39:34 +0530

    Driver Code

--- Dynamic Programming And Greedy For Beginners/Maximum Sum Non Adjacent Elements.java
+++ Dynamic Programming And Greedy For Beginners/Maximum Sum Non Adjacent Elements.java
@@ -1,1 +1,10 @@
 // https://nados.io/question/maximum-sum-non-adjacent-elements
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
