beab40f8541cb66ea427d8804d7549cf802fad67
ParveendThakur <parveend1234@gmail.com>
2022-08-02 00:09:16 +0530

    done Q--- Submit

--- Dynamic Programming And Greedy For Beginners/Coin Change Permutations.java
+++ Dynamic Programming And Greedy For Beginners/Coin Change Permutations.java
@@ -7,4 +7,32 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
     }
+}import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int [] coins = new int[n];
+        for(int i = 0; i<n; i++){
+            coins[i] = scn.nextInt();
+        }
+        int tar = scn.nextInt();
+
+        int[] dp = new int [tar+1];
+        dp[0] = 1;
+        for (int i =1; i<=tar; i++){
+            for(int j= 0; j<n; j++){
+                int coin = coins[j];
+                if(coin<=i){
+                    dp[i] += dp[i-coin];
+                }
+            }
+        }
+
+        System.out.println(dp[tar]);
+
+    }
 }
