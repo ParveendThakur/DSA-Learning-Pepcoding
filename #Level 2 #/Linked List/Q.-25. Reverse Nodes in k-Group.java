94fed82a646a464a88929331c750e9d1fe12bec1
ParveendThakur <parveend1234@gmail.com>
2022-08-06 22:57:43 +0530

    done in -- H/W .. ses....

--- #Level 2 #/Linked List/Q.-25. Reverse Nodes in k-Group.java
+++ #Level 2 #/Linked List/Q.-25. Reverse Nodes in k-Group.java
@@ -1,4 +1,3 @@
-// https://leetcode.com/problems/reverse-nodes-in-k-group/
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -10,8 +9,44 @@
  * };
  */
 class Solution {
-public:
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        
+    public ListNode reverse(ListNode head){
+        if(head==null || head.next==null) return head;
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode nbr = curr.next;
+            curr.next= prev;
+            prev = curr;
+            curr = nbr;     
+        }
+        return prev;
     }
-};
+    
+    
+    public ListNode reverseKGroup(ListNode head , int k){
+        ListNode temp = head;
+         int s =0;
+         while (temp!=null){
+             temp = temp.next;
+             s++;   
+         }
+        int wk = s/k;
+        ListNode dummy = new ListNode(0);
+        ListNode p1 = dummy ;
+        ListNode curr = head;
+        ListNode prev= head;
+        while(wk-->0){
+            int time = k-1;
+            while(time-->0) curr = curr.next;
+            ListNode nbr = curr.next;
+            curr.next= null;
+            
+            ListNode rev = reverse(prev);
+            p1.next = curr;
+            p1 = prev;
+            curr = prev = nbr;
+        }
+        p1.next = curr;
+        return dummy.next;
+    }
+}
