b9e8f3d736a4954f1eb50dc33d8765bd76f513cd
ParveendThakur <parveend1234@gmail.com>
2022-08-18 00:17:06 +0530

    driver code

--- #Level 2 #/HashMap And Heap For Intermediate/Count Of All Subarrays With Zero Sum.java
+++ #Level 2 #/HashMap And Heap For Intermediate/Count Of All Subarrays With Zero Sum.java
@@ -1,1 +1,23 @@
 //https://nados.io/question/count-of-all-subarrays-with-zero-sum
+import java.util.*;
+
+public class Main {
+
+	public static int solution(int[] arr) {
+		// write your code here
+
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = scn.nextInt();
+		}
+		System.out.println(solution(arr));
+
+	}
+
+}
