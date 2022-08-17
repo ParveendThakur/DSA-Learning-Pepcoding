bab8ce5845eaef41f03e0b80096b284e8860efed
ParveendThakur <parveend1234@gmail.com>
2022-08-17 21:30:28 +0530

    driver code

--- #Level 2 #/HashMap And Heap For Intermediate/Number Of Employees Under Every Manager.java
+++ #Level 2 #/HashMap And Heap For Intermediate/Number Of Employees Under Every Manager.java
@@ -1,1 +1,14 @@
 // https://nados.io/question/number-of-employees-under-every-manager
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    
+    //write your code here
+  }
+
+}
