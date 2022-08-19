69368909c799f326df01663474612d46a3851347
ParveendThakur <parveend1234@gmail.com>
2022-08-20 00:15:47 +0530

    driver code

--- #Level 2 #/HashMap And Heap For Intermediate/Longest Subarray With Equal Number Of Zeroes And Ones.java
+++ #Level 2 #/HashMap And Heap For Intermediate/Longest Subarray With Equal Number Of Zeroes And Ones.java
@@ -1,1 +1,21 @@
 //https://nados.io/question/longest-subarray-with-equal-number-of-zeroes-and-ones
+import java.util.*;
+
+public class Main {
+
+    public static int solution(int[] arr) {
+        // write your code here
+
+        return 0;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        System.out.println(solution(arr));
+    }
+
+}
