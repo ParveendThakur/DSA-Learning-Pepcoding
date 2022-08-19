19c9ab539a2d91d86e62267b6c12a1ffcbf36ce3
ParveendThakur <parveend1234@gmail.com>
2022-08-20 00:17:06 +0530

    driver code

--- #Level 2 #/HashMap And Heap For Intermediate/Count Of Subarrays With Equal Number Of 0s 1s And 2s.java
+++ #Level 2 #/HashMap And Heap For Intermediate/Count Of Subarrays With Equal Number Of 0s 1s And 2s.java
@@ -1,1 +1,22 @@
 // https://nados.io/question/count-of-subarrays-with-equal-number-of-0s-1s-and-2s
+import java.util.*;
+
+public class Main {
+
+    public static int solution(int[] arr) {
+        // write your code here
+
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        System.out.println(solution(arr));
+    }
+
+}
