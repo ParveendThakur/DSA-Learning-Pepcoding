e9ebcd6a893d683d86b92f77f57a0dbcf0ef5046
ParveendThakur <parveend1234@gmail.com>
2022-08-19 22:55:21 +0530

    driver code

--- #Level 2 #/HashMap And Heap For Intermediate/Longest Subarray With Sum Divisible By K.java
+++ #Level 2 #/HashMap And Heap For Intermediate/Longest Subarray With Sum Divisible By K.java
@@ -1,1 +1,23 @@
 // https://nados.io/question/longest-subarray-with-sum-divisible-by-k
+import java.util.*;
+
+public class Main {
+
+    public static int solution(int[] arr, int k) {
+        // write your code here
+
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int k = scn.nextInt();
+        System.out.println(solution(arr, k));
+    }
+
+}
