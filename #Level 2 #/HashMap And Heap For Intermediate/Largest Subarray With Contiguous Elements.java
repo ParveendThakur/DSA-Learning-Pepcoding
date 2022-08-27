732de270c4843ff72099a0039261defe2c07ddc0
ParveendThakur <parveend1234@gmail.com>
2022-08-27 20:43:44 +0530

    Q.-- discussed in class-- H/W..

--- #Level 2 #/HashMap And Heap For Intermediate/Largest Subarray With Contiguous Elements.java
+++ #Level 2 #/HashMap And Heap For Intermediate/Largest Subarray With Contiguous Elements.java
@@ -1,1 +1,21 @@
 // https://nados.io/question/largest-subarray-with-contiguous-elements
+import java.util.*;
+
+public class Main {
+
+	public static int solution(int[] arr) {
+		// write your code here
+
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int[] arr = new int[scn.nextInt()];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scn.nextInt();
+		}
+		System.out.println(solution(arr));
+	}
+
+}
