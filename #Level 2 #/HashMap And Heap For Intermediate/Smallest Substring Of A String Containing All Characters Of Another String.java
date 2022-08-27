11cc2204bab725cc5923d54acd3e889549c48a47
ParveendThakur <parveend1234@gmail.com>
2022-08-27 20:48:28 +0530

    driver code--- in class-- acquire & release---H/W

--- #Level 2 #/HashMap And Heap For Intermediate/Smallest Substring Of A String Containing All Characters Of Another String.java
+++ #Level 2 #/HashMap And Heap For Intermediate/Smallest Substring Of A String Containing All Characters Of Another String.java
@@ -1,1 +1,19 @@
 // https://nados.io/question/smallest-substring-of-a-string-containing-all-characters-of-another-string
+import java.util.*;
+
+public class Main {
+
+	public static String solution(String s1, String s2){
+		// write your code here
+		
+		return null;
+	}
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String s1 = scn.next();
+		String s2 = scn.next();
+		System.out.println(solution(s1,s2));
+	}
+
+}
