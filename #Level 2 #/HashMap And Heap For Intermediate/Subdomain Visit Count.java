ee08e7ffc58907f5632f67ec168b69e10e68d1d8
ParveendThakur <parveend1234@gmail.com>
2022-08-27 20:51:12 +0530

    driver code- in class--normal question + split a string (Split())-- H/W

--- #Level 2 #/HashMap And Heap For Intermediate/Subdomain Visit Count.java
+++ #Level 2 #/HashMap And Heap For Intermediate/Subdomain Visit Count.java
@@ -1,1 +1,24 @@
 // https://nados.io/question/subdomain-visit-count
+import java.util.*;
+
+public class Main {
+    
+	public static List<String> subdomainVisits(String[] cpdomains) {
+		// write your code here
+
+		return null;
+	}
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = Integer.parseInt(s.nextLine());
+		String[] cpd = new String[n];
+		for (int i = 0; i < cpd.length; i++) {
+			cpd[i] = s.nextLine();
+		}
+		List<String> ans = subdomainVisits(cpd);
+		Collections.sort(ans);
+		System.out.println(ans);
+	}
+
+}
