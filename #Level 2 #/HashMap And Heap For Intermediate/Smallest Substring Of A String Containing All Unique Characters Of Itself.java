c021f5610a5c2d268799ca51817f97541feeec55
ParveendThakur <parveend1234@gmail.com>
2022-08-27 20:46:35 +0530

    driver code add--- done in class by sir

--- #Level 2 #/HashMap And Heap For Intermediate/Smallest Substring Of A String Containing All Unique Characters Of Itself.java
+++ #Level 2 #/HashMap And Heap For Intermediate/Smallest Substring Of A String Containing All Unique Characters Of Itself.java
@@ -1,1 +1,18 @@
 // https://nados.io/question/smallest-substring-of-a-string-containing-all-unique-characters-of-itself
+import java.util.*;
+
+public class Main {
+
+	public static int solution(String str){
+		// write your code here
+		
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(solution(str));
+	}
+
+}
